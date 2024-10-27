2baaa8440c7ed039d164ca69a8f130dad784e65d
rodmendietam <dmendietamendez@gmail.com>
2024-10-26 18:40:52 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("42");
+
+console.log("`Veinticinco`");
+
+console.log(-666);
+
+console.log(5 === 5);
+
+console.log(5-5);
+
+console.log(".");
+
+console.log("null");
+
+console.log("undefined");
+
+console.log(5 ===4);
+
